f2f2a0ade0bac9d08a87ff8c946c4fc3ab0f5331
louiscklaw <louiscklaw@gmail.com>
2020-07-27 21:09:48 +0800

    update to add more test,

--- unittest-json-reporting-tryout/main.py
+++ unittest-json-reporting-tryout/main.py
@@ -20,37 +20,57 @@ def run_test():
 
 class TestSequenceFunctions(unittest.TestCase):
 
-  def setUp(self):
-    self.seq = list(range(10))
+    def setUp(self):
+        self.seq = list(range(10))
 
-  @unittest.skip("demonstrating skipping")
-  def test_skipped(self):
-    self.fail("shouldn't happen")
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
-  def test_passed(self):
-    self.assertTrue(True,'test_passed should be always passed')
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
 
-  def test_failed(self):
-    self.assertFalse(True,'test_failed should be always failed')
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 
-  def test_shuffle(self):
-    # make sure the shuffled sequence does not lose any elements
-    random.shuffle(self.seq)
-    self.seq.sort()
-    self.assertEqual(self.seq, list(range(10)))
+    def test_error(self):
+        """ This test should be marked as error one. """
+        raise ValueError
 
-    # should raise an exception for an immutable sequence
-    self.assertRaises(TypeError, random.shuffle, (1,2,3))
+    def test_fail(self):
+        """ This test should fail. """
+        self.assertEqual(1, 2)
 
-  def test_choice(self):
-    element = random.choice(self.seq)
-    self.assertTrue(element in self.seq)
+    @unittest.skip("This is a skipped test.")
+    def test_skip(self):
+        """ This test should be skipped. """
+        pass
+    @unittest.skip("demonstrating skipping")
+    def test_skipped(self):
+        self.fail("shouldn't happen")
 
-  def test_sample(self):
-    with self.assertRaises(ValueError):
-      random.sample(self.seq, 20)
-    for element in random.sample(self.seq, 5):
-      self.assertTrue(element in self.seq)
+    def test_shuffle(self):
+        # make sure the shuffled sequence does not lose any elements
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, list(range(10)))
+
+        # should raise an exception for an immutable sequence
+        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+
+    def test_choice(self):
+        element = random.choice(self.seq)
+        self.assertTrue(element in self.seq)
+
+    def test_sample(self):
+        with self.assertRaises(ValueError):
+            random.sample(self.seq, 20)
+        for element in random.sample(self.seq, 5):
+            self.assertTrue(element in self.seq)
 
 if __name__ == '__main__':
   run_test()
