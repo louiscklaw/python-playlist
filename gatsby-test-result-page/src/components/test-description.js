5e5054e18424e62e65ec50c9380d1eae17a41ef1
louiscklaw <louiscklaw@gmail.com>
2020-07-30 01:28:32 +0800

    update for working custHeaderStyle,

--- gatsby-test-result-page/src/components/test-description.js
+++ gatsby-test-result-page/src/components/test-description.js
@@ -5,8 +5,35 @@ import HtmlToReact from  'html-to-react'
 import ReactMarkdown from 'react-markdown'
 import htmlParser from 'react-markdown/plugins/html-parser'
 
-function MyStyle(level, props){
-  return "hello mystyle"
+import {combineStyle} from '../utils/common'
+
+import style from '../scss/style.module.scss'
+
+function custHeaderStyle(level, props){
+  switch (level) {
+    case 1:
+      console.log(props)
+      return (<h1 className={combineStyle([style.title, style.is4])}>{props.children}</h1>)
+    case 2:
+      console.log(props)
+      return (<h2 className={combineStyle([style.subtitle, style.is4])}>{props.children}</h2>)
+    case 3:
+      console.log(props)
+      return (<h3 className={combineStyle([style.title, style.is5])}>{props.children}</h3>)
+    case 4:
+      console.log(props)
+      return (<h4 className={combineStyle([style.subtitle, style.is5])}>{props.children}</h4>)
+    case 5:
+      console.log(props)
+      return (<h5 className={combineStyle([style.title, style.is6])}>{props.children}</h5>)
+    case 6:
+      console.log(props)
+      return (<h6 className={combineStyle([style.subtitle, style.is6])}>{props.children}</h6>)
+
+    default:
+      return "hello custHeaderStyle"
+      break;
+  }
 }
 
 function TestDescription(props){
@@ -36,7 +63,7 @@ function TestDescription(props){
         source={markdown}
         escapeHtml={false}
         renderers={{
-        heading: props => MyStyle(props.level, props)
+        heading: props => custHeaderStyle(props.level, props)
 
         }}
       />
