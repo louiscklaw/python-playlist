d2a17c057a62e4d724c907a840b8f243019a063c
louiscklaw <louiscklaw@gmail.com>
2020-08-04 20:43:39 +0800

    update to run result page generation,

--- unittest-json-reporting-tryout/src/run.sh
+++ unittest-json-reporting-tryout/src/run.sh
@@ -12,15 +12,13 @@ pipenv run python3 before_test.py
 
 mkdir -p $REPORT_DIR
 
-pipenv run python3 test/unit/test_unit_run.py &
-pipenv run python3 test/integration/test_integration_run.py &
-pipenv run python3 test/sanity/test_sanity_run.py &
-pipenv run python3 test/smoke/test_smoke_run.py &
-pipenv run python3 test/interface/test_interface_run.py &
-pipenv run python3 test/regression/test_regression_run.py &
-pipenv run python3 test/acceptance/test_acceptance_run.py &
-
-wait
+pipenv run python3 test/unit/test_unit_run.py
+pipenv run python3 test/integration/test_integration_run.py
+pipenv run python3 test/sanity/test_sanity_run.py
+pipenv run python3 test/smoke/test_smoke_run.py
+pipenv run python3 test/interface/test_interface_run.py
+pipenv run python3 test/regression/test_regression_run.py
+pipenv run python3 test/acceptance/test_acceptance_run.py
 
 set -ex
 
