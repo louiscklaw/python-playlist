822c29976867df661ffa399045724c0ea2092a08
louiscklaw <louiscklaw@gmail.com>
2020-08-04 21:27:56 +0800

    update,

--- scripts/before_commit.sh
+++ scripts/before_commit.sh
@@ -3,5 +3,6 @@
 set -ex
 
 rm -rf unittest-json-reporting-tryout/test-reports
+git rm -rf unittest-json-reporting-tryout/test-reports/*.xml
 
 git checkout unittest-json-reporting-tryout/test-reports
--- unittest-json-reporting-tryout/src/run.sh
+++ unittest-json-reporting-tryout/src/run.sh
@@ -6,29 +6,29 @@ export SRC_DIR=$PWD
 export PROJ_HOME=$PWD/..
 export REPORT_DIR=$PROJ_HOME/test-reports
 
-pipenv sync
+# pipenv sync
 
-pipenv run python3 before_test.py
+# pipenv run python3 before_test.py
 
-mkdir -p $REPORT_DIR
+# mkdir -p $REPORT_DIR
 
 pipenv run python3 test/unit/test_unit_run.py
-pipenv run python3 test/integration/test_integration_run.py
-pipenv run python3 test/sanity/test_sanity_run.py
-pipenv run python3 test/smoke/test_smoke_run.py
-pipenv run python3 test/interface/test_interface_run.py
-pipenv run python3 test/regression/test_regression_run.py
-pipenv run python3 test/acceptance/test_acceptance_run.py
+# pipenv run python3 test/integration/test_integration_run.py
+# pipenv run python3 test/sanity/test_sanity_run.py
+# pipenv run python3 test/smoke/test_smoke_run.py
+# pipenv run python3 test/interface/test_interface_run.py
+# pipenv run python3 test/regression/test_regression_run.py
+# pipenv run python3 test/acceptance/test_acceptance_run.py
 
 set -ex
 
 pipenv run python3 after_test.py
 
-pipenv run python3 ./attach_docstring.py
+# pipenv run python3 ./attach_docstring.py
 
-pipenv run python3 ./attach_testsuite_docstring.py
+# pipenv run python3 ./attach_testsuite_docstring.py
 
-pipenv run python3 ./attach_reports_meta.py
+# pipenv run python3 ./attach_reports_meta.py
 
 
 # below is the code for report generation
