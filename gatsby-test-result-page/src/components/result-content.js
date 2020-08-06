1a9235cee0fbda459deec6e28137515d181eb9a5
louiscklaw <louiscklaw@gmail.com>
2020-08-06 15:03:18 +0800

    update,

--- gatsby-test-result-page/src/components/result-content.js
+++ gatsby-test-result-page/src/components/result-content.js
@@ -1,15 +1,50 @@
 import React from 'react'
 
-function ResultContent(props){
+import PreJson from '~components/pre-json'
+import PreUnittest from '~components/pre-unittest'
+
+import GlobalContext from '~contexts/global'
+
+function Loading(){
   return(
     <>
-      result content
-      {
-        JSON.stringify(props.test_type, null, 2)
-      }
+      Loading
     </>
   )
 }
 
+function ResultContent(props){
+  let {python_test_result} = React.useContext(GlobalContext)
+  let [test_result_show_here, setTestResultShowHere] = React.useState(null)
+
+  React.useEffect(()=>{
+    setTestResultShowHere(Loading())
+  },[])
+
+  React.useEffect(()=>{
+    let test_type = props.test_type
+    let test_type_in_report = test_type
+    let test_type_found = Object.keys(python_test_result).indexOf(test_type_in_report) > -1
+
+    if (test_type_found){
+      // grep the specific result by test type
+      let test_type_result_json = python_test_result[test_type_in_report]
+      setTestResultShowHere(JSON.stringify(test_type_result_json, null, 2))
+    }else{
+      setTestResultShowHere( 'the wanted test type not found'+JSON.stringify({}) )
+    }
+
+    console.log(props.test_type)
+    console.log(python_test_result[test_type])
+
+  },[python_test_result])
+
+
+  return(
+    <>
+      { test_result_show_here }
+    </>
+  )
+}
 
 export default ResultContent
--- gatsby-test-result-page/src/pages/app.js
+++ gatsby-test-result-page/src/pages/app.js
@@ -2,16 +2,10 @@ import React from "react"
 import {Link} from 'gatsby'
 
 import { Router } from "@reach/router"
+
 import Layout from "../components/layout"
-
-
 import Navbar from '~components/nav'
-import PreJson from '~components/pre-json'
-import PreUnittest from '~components/pre-unittest'
 import ResultContent from '~components/result-content'
-
-import GlobalContext from '~contexts/global'
-
 
 const App = () => {
   return (
