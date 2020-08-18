a193e805a89870137879c30998057a5340b668b8
louiscklaw <louiscklaw@gmail.com>
2020-08-18 18:07:31 +0800

    update,

--- gatsby-test-result-page/gatsby-browser.js
+++ gatsby-test-result-page/gatsby-browser.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from "react"
 
-// You can delete this file if you're not using it
+import { ThemeContextProvider } from "./src/contexts/ThemeContext"
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeContextProvider>
+    {element}
+  </ThemeContextProvider>
+)
--- gatsby-test-result-page/src/components/nav/index.js
+++ gatsby-test-result-page/src/components/nav/index.js
@@ -1,72 +1,214 @@
 import React from 'react'
 import {Link} from 'gatsby'
 
-import style from '../../scss/style.module.scss'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faBug } from '@fortawesome/free-brands-svg-icons'
+import {
+  fabGithub,
+  fasBug,
+  fasPalette,
+  fasQuestionCircle,
+  fasRoad,
+  fasCodeBranch,
+  fasClock,
+  fasBook
+} from '@fortawesome/free-brands-svg-icons'
 
-const combineStyle = (styles) => styles.join(' ')
+import ThemeContext from '../../contexts/ThemeContext'
 
-function Navbar(props){
+const combineStyles = (styles) => styles.join(' ')
+
+function NavIndex(props){
+  const {active_style, rotateStyle} = React.useContext(ThemeContext)
+
   return(
-    <nav className={style.navbar} role="navigation" aria-label="main navigation">
-      <div className={style.navbarBrand}>
-
-        <a className={style.navbarItem} href="dashboard.html">
-          <FontAwesomeIcon icon={faGithub} size="2x" />
-          Simple test report
+    <nav className={active_style.navbar} role="navigation" aria-label="main navigation">
+  <div className={active_style.navbarBrand}>
+    <a className={active_style.navbarItem} href="overview.html">
+      Simple test report
+    </a>
+    <a role="button" className={combineStyles([active_style.navbarBurger, active_style.burger])} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </a>
+  </div>
+  <div id="navbarBasicExample" className={active_style.navbarMenu}>
+    <div className={active_style.navbarStart}>
+      <a className={combineStyles([active_style.navbarItem, active_style.isActive])} href="dashboard.html">
+        Overview
+      </a>
+      <a className={active_style.navbarItem} href="test-report-content.html">
+        smoke_test </a>
+      <a className={active_style.navbarItem}>
+        unit_test
+      </a>
+      <a className={active_style.navbarItem}>
+        integration_test
+      </a>
+      <a className={active_style.navbarItem}>
+        acceptance_test
+      </a>
+      <a className={active_style.navbarItem}>
+        interface_test
+      </a>
+      <a className={active_style.navbarItem}>
+        regression_test
+      </a>
+      <a className={active_style.navbarItem}>
+        sanity_test
+      </a>
+      <div className={combineStyles([active_style.navbarItem, active_style.hasDropdown, active_style.isHoverable])}>
+        <a className={active_style.navbarLink}>
+          More
         </a>
-
-        <a role="button" className={[style.navbarBurger, style.burger]} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" className={style.navbarMenu}>
-        <div className={style.navbarStart}>
-
-          <Link to="/overview" className={style.navbarItem} >Overview</Link>
-          <Link to="/smoke_test" className={style.navbarItem}>smoke_test</Link>
-          <Link to="/unit_test" className={style.navbarItem}>unit_test</Link>
-          <Link to="/integration_test" className={style.navbarItem}>integration_test</Link>
-          <Link to="/acceptance_test" className={style.navbarItem}>acceptance_test</Link>
-          <Link to="/interface_test" className={style.navbarItem}>interface_test</Link>
-          <Link to="/regression_test" className={style.navbarItem}>regression_test</Link>
-          <Link to="/sanity_test" className={style.navbarItem}>sanity_test</Link>
-
-
-          <a className={style.navbarItem}> Documentation </a>
-
-          <div className={combineStyle([style.navbarItem, style.hasDropdown, style.isHoverable])}>
-            <a className={style.navbarLink}>
-              More
-            </a>
-
-            <div className={style.navbarDropdown}>
-
-              <a className={style.navbarItem}> About </a>
-
+        <div className={active_style.navbarDropdown}>
+          {/* navbar-item */}
+          <a href="time_analysis.html" className={active_style.navbarItem}>
+            <div className={combineStyles([active_style.level, active_style.isMobile])}>
+              <div className={active_style.levelLeft}>
+                <div className={active_style.levelItem}>
+                  <p>
+                    <FontAwesomeIcon icon={fasBook} />
+                    <strong>Documentation</strong>
+                    <br />
+                    <small>time used for the tests</small>
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div className={style.navbarEnd}>
-          <div className={style.navbarItem}>
-            <div className={style.buttons}>
-              <a className={combineStyle([style.button, style.isWhite])}>
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-                <p style={{margin: '0 0.3rem'}}>Github source</p>
-              </a>
+          </a>
+          {/* navbar-item */}
+          {/* navbar-item */}
+          <a href="time_analysis.html" className={active_style.navbarItem}>
+            <div className={combineStyles([active_style.level, active_style.isMobile])}>
+              <div className={active_style.levelLeft}>
+                <div className={active_style.levelItem}>
+                  <p>
+                    <FontAwesomeIcon icon={fasClock} />
+                    <strong>time analysis</strong>
+                    <br />
+                    <small>time used for the tests</small>
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
+          </a>
+          {/* navbar-item */}
+          {/* navbar-item */}
+          <a href="chartjs_test.html" className={active_style.navbarItem}>
+            <div className={combineStyles([active_style.level, active_style.isMobile])}>
+              <div className={active_style.levelLeft}>
+                <div className={active_style.levelItem}>
+                  <p>
+                    <FontAwesomeIcon icon={fasCodeBranch} />
+                    <strong>chartjs test</strong>
+                    <br />
+                    <small>back stage to test chartjs api</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
+          {/* navbar-item */}
+          {/* navbar-item */}
+          <a href="roadmap.html" className={active_style.navbarItem}>
+            <div className={combineStyles([active_style.level, active_style.isMobile])}>
+              <div className={active_style.levelLeft}>
+                <div className={active_style.levelItem}>
+                  <p>
+                    <FontAwesomeIcon icon={fasRoad} />
+                    <strong>Roadmap</strong>
+                    <br />
+                    <small>simple plan of this test report</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
+          {/* navbar-item */}
+          {/* navbar-item */}
+          <a href="about.html" className={active_style.navbarItem}>
+            <div className={combineStyles([active_style.level, active_style.isMobile])}>
+              <div className={active_style.levelLeft}>
+                <div className={active_style.levelItem}>
+                  <p>
+                    <FontAwesomeIcon icon={fasQuestionCircle} />
+                    <strong>About this page</strong>
+                    <br />
+                    <small>Side projects to enhance Bulma</small>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
+          {/* navbar-item */}
         </div>
-
       </div>
-    </nav>
+      <div className={active_style.navbarItem}>
+        <div className={combineStyles([active_style.select, active_style.isSmall])} style={{color: '#34495e'}}>
+          <select>
+            <option>Select version</option>
+            <option>options version 1</option>
+            <option>options version 2</option>
+            <option>options version 3</option>
+            <option>options version 4</option>
+            <option>options version 5</option>
+            <option>options version 6</option>
+            <option>options version 7</option>
+            <option>options version 8</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div className={active_style.navbarEnd}>
+      <div className={active_style.navbarItem}>
+        <div className={active_style.buttons}>
+          {/* navbar-item */}
+          <a href="about.html" className={active_style.navbarItem}>
+            <div className={combineStyles([active_style.level, active_style.isMobile])}>
+              <div className={active_style.levelLeft}>
+                <div className={active_style.levelItem}>
+                  <p>
+                    <FontAwesomeIcon icon={fasPalette} />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
+          {/* navbar-item */}
+          {/* navbar-item */}
+          <a href="about.html" className={active_style.navbarItem}>
+            <div className={combineStyles([active_style.level, active_style.isMobile])}>
+              <div className={active_style.levelLeft}>
+                <div className={active_style.levelItem}>
+                  <p>
+                    <FontAwesomeIcon icon={fasBug} />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
+          {/* navbar-item */}
+          {/* navbar-item */}
+          <a href="about.html" className={active_style.navbarItem}>
+            <div className={combineStyles([active_style.level, active_style.isMobile])}>
+              <div className={active_style.levelLeft}>
+                <div className={active_style.levelItem}>
+                  <p>
+                    <FontAwesomeIcon icon={fabGithub} />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </a>
+          {/* navbar-item */}
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
   )
 }
 
-export default Navbar
+export default NavIndex
