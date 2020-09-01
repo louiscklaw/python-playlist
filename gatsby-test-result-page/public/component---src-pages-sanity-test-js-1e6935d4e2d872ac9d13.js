a929f8c9a758cec31d2c89b99850fa2bbc455b44
louiscklaw <louiscklaw@gmail.com>
2020-09-01 13:44:36 +0800

    update and test,

---
+++ gatsby-test-result-page/public/component---src-pages-sanity-test-js-1e6935d4e2d872ac9d13.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{UXkj:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),r=t.n(a);e.default=function(n){return r.a.createElement(r.a.Fragment,null,"test page")}}}]);
+//# sourceMappingURL=component---src-pages-sanity-test-js-1e6935d4e2d872ac9d13.js.map
