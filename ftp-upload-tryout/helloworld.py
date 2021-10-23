f4e7a89cbce5611ee0c9df687ae9bb69ead32101
louiscklaw <louiscklaw@gmail.com>
2021-10-23 12:56:31 +0800

    update,

--- ftp-upload-tryout/helloworld.py
+++ ftp-upload-tryout/helloworld.py
@@ -9,6 +9,8 @@ PUREUDON_FTP_PASSWORD = os.getenv('PUREUDON_FTP_PASSWORD')
 PUREUDON_FTP_USERNAME = os.getenv('PUREUDON_FTP_USERNAME')
 PUREUDON_HOMEPAGE_PATH = os.getenv('PUREUDON_HOMEPAGE_PATH')
 
+
+
 session = ftplib.FTP(PUREUDON_FTP_HOST,PUREUDON_FTP_USERNAME,PUREUDON_FTP_PASSWORD)
 file = open('index.html','rb')                  # file to send
 session.storbinary('STOR index.html', file)     # send the file
