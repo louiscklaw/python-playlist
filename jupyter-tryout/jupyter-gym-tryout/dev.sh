e7c9bea20eff60d0439c93739a16f61cb26fe189
Louis Law <louiscklaw@users.noreply.github.com>
2023-05-05 17:57:24 +0800

    update jupyter-gym-tryout,

---
+++ jupyter-tryout/jupyter-gym-tryout/dev.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -ex
+
+pip install jupyter
+pip install gym 
+pip install gym-retro
+
+# pushd roms
+#   python -m retro.import .
+# popd
+
+# jupyter notebook \
+#   --allow-root \
+#   --ip=0.0.0.0 \
+#   --notebook-dir=notebook
