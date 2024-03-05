4bd228c3aaad3d0448541002bcd52fb846e41089
Louis Law, louiscklaw <louiscklaw@users.noreply.github.com>
2024-03-05 16:11:19 +0800

    'update,'

--- hugging-chat/api/resources/AskQuestion.py
+++ hugging-chat/api/resources/AskQuestion.py
@@ -1,48 +1,45 @@
 from flask import Flask, request, jsonify
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-
-from flask_restful import  Resource
-
-from flask_sqlalchemy import SQLAlchemy
-
-import hashlib
-
-from datetime import datetime
+from flask_restful import Api, Resource
 
 from lib.HuggingChatProcessor import HuggingChatProcessor
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from _poc.nocodb.create_ask_record import create_ask_record
 
-limiter = Limiter(
-    get_remote_address,
-    app=app,
-    default_limits=["200 per day"],
-    storage_uri="memory://",
-)
+# Define the NoCodeDB configuration
+host = '192.168.10.61'
+port = 8081
+api_key = 'TdryW6UByeXUqjZfa0AdRb3GIeebj8IwltDYmAUu'
+table_id = 'm90fau872vtveh6'
+api_endpoint = f'http://{host}:{port}/api/v2'
+
+app = Flask(__name__)
+api = Api(app)
 
 class AskQuestionResource(Resource):
-    @limiter.limit("3/minute", override_defaults=True)
+    def get(self):
+        """Returns a greeting message"""
+        response = { "message": "Get Hello, world!" }
+        return jsonify(response)
+    
     def post(self):
-      print('ask_question')
-      ip = request.remote_addr
-      user_agent = request.headers.get("User-Agent")
+        """Returns a greeting message"""
 
-      if not request.is_json:
-        return jsonify({"error": "Missing JSON in request"}), 400
-      
-      data = request.get_json()
-      try:
-        question = data["question"]
-        pre_prompt = data['pre_prompt']
+        ips = []
+        if hasattr(request, 'access_route'):
+            # For Flask < 2.0 compatibility
+            ips += request.access_route
+            
+        ips.append(request.remote_addr)
+        ip = ', '.join(ips)
+        user_agent = request.user_agent.string
+        json_body = request.get_json()
+        question = json_body['question']
+        pre_prompt = json_body['pre_prompt']
+
         my_instance = HuggingChatProcessor()
         answer = my_instance.get_answer(question, pre_prompt)
-
-        print(question)
         print(answer)
 
-        return jsonify({'status': 'success', 'answer': answer.text}), 200
-      except KeyError as e:
-        return jsonify({"error": str(e)}), 400
+        create_ask_record(ip,user_agent,pre_prompt,question, answer.text, json_body)
+
+        return jsonify({"message": answer.text})
--- hugging-chat/api/resources/Helloworld.py
+++ hugging-chat/api/resources/Helloworld.py
@@ -1,5 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
+
+from _poc.nocodb.create_ask_record import create_ask_record
+
+# Define the NoCodeDB configuration
+host = '192.168.10.61'
+port = 8081
+api_key = 'TdryW6UByeXUqjZfa0AdRb3GIeebj8IwltDYmAUu'
+table_id = 'm90fau872vtveh6'
+api_endpoint = f'http://{host}:{port}/api/v2'
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,8 +16,23 @@ api = Api(app)
 class HelloWorldResource(Resource):
     def get(self):
         """Returns a greeting message"""
-        return {"message": "Get Hello, world!"}
+        response = { "message": "Get Hello, world!" }
+        return jsonify(response)
     
     def post(self):
         """Returns a greeting message"""
-        return {"message": "Post Hello, world!"}
+
+        ips = []
+        if hasattr(request, 'access_route'):
+            # For Flask < 2.0 compatibility
+            ips += request.access_route
+            
+        ips.append(request.remote_addr)
+        ip = ', '.join(ips)
+        user_agent = request.user_agent.string
+        json_body = request.get_json()
+        question = json_body['question']
+        pre_prompt = json_body['pre_prompt']
+
+        create_ask_record(ip,user_agent,pre_prompt,question,'a', json_body)
+        return jsonify({"message": "Post Hello, world!"})
